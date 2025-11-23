9fc627a7215a52d9cc51857617ebc01b98e75099
Ant0CPU <antonio.bruno@marconibari.edu.it>
2025-11-23 18:50:07 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 using namespace std;
 
+int maximum (int x, int y) {
+    int r;
+
+    if (x>y) {
+        r=x;
+    }
+    else
+    {
+        r=y;
+    }
+
+    return r;
+}
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int n1, n2, n3, n4, max;
+
+    cout<<"Calcolo del massimo fra 4 numeri"<<endl;
+    cout<<"Inserisci 4 valori"<<endl;
+    cin>>n1>>n2>>n3>>n4;
+
+    max=maximum(n1, n2);
+    max=maximum(max, n3);
+    max=maximum(max, n4);
+
+    cout<<"Il valore massimo è "<<max<<endl;
+
+    return 0;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
